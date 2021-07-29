6e2a1f8dd3427d910f90586be71b9798b4e91039
wang14597 <1459703155@qq.com>
2021-07-29 18:04:07 +0800

    add JDBCUtils

---
+++ src/main/java/JDBC/JDBCUtils.java
@@ -0,0 +1,90 @@
+package JDBC;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.*;
+import java.util.Properties;
+
+/**
+ * JDBC工具类
+ */
+public class JDBCUtils {
+    private static String url;
+    private static String user;
+    private static String password;
+    private static String driver;
+
+    /**
+     * 文件的读取，只需要读取一次即可拿到这些值（使用静态代码块）
+     */
+    static {
+        try {
+            Properties properties = new Properties();
+
+            ClassLoader classLoader = JDBCUtils.class.getClassLoader();
+            URL resource = classLoader.getResource("jdbc.properties");
+            String path = resource.getPath();
+            properties.load(new FileReader(path));
+//            properties.load(new FileReader("src/jdbc.properties"));
+            url = properties.getProperty("url");
+            user = properties.getProperty("user");
+            password = properties.getProperty("password");
+            driver = properties.getProperty("driver");
+            Class.forName(driver);  // 注册数据库驱动
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 获取连接
+     *
+     * @return 连接对象
+     */
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.
+                getConnection(url, user, password);
+    }
+
+    public static void close(ResultSet resultSet, Statement stmt, Connection connection) {
+        if (resultSet != null) {
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (stmt != null) {
+            try {
+                stmt.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void close(Statement stmt, Connection connection) {
+        if (stmt != null) {
+            try {
+                stmt.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
